b21a8fc49da776b4127734c8760d87398dee9ccc
yuvanvk <abhivigneshofficial@gmail.com>
2026-05-19 16:02:57 +0530

    feat: add markdown parser

--- apps/www/components/card/message.card.tsx
+++ apps/www/components/card/message.card.tsx
@@ -2,13 +2,12 @@
 
 import { motion } from "motion/react";
 import { Message } from "@workspace/types";
+import ReactMarkdown from "react-markdown";
 import { cn } from "@workspace/ui/lib/utils";
-import Image from "next/image";
 
 export function MessageCard({ id, content, role }: Message) {
   const isUser = role === "user";
   const isAssistant = role === "assistant";
-  const isLatestAiMessage = id === "new-ai-message"
 
   return (
     <motion.div
@@ -19,16 +18,41 @@ export function MessageCard({ id, content, role }: Message) {
         ease: "easeOut",
       }}
       className={cn(
-        isUser && "w-fit max-w-xs ml-auto px-2 py-1.5 bg-neutral-800 rounded-xl",
+        isUser &&
+        "w-fit max-w-xs ml-auto px-2 py-1.5 bg-neutral-800 rounded-xl",
         isAssistant && "max-w-2xl w-full mr-auto p-1.5",
-        "my-2 text-neutral-100 text-[15px]",
+        "mb-3 text-neutral-100 text-[15px]",
       )}
     >
       {isUser && content}
-      {isAssistant && <span>
-        AI
-        {content}
-      </span>}
+      {isAssistant && (
+        <div>
+          {!content && (
+            <motion.span
+              className="bg-linear-to-r from-zinc-500 via-zinc-100 to-zinc-500 bg-clip-text text-transparent text-[13px] flex items-center gap-1"
+              style={{
+                backgroundSize: "200% 100%",
+              }}
+              animate={{
+                backgroundPositionX: ["0%", "200%"],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                ease: "linear",
+              }}
+            >
+              <img src={"/logo.svg"} className="w-4 h-4" />
+              Thinking…
+            </motion.span>
+          )}
+          {content &&
+            <ReactMarkdown>
+              {content}
+            </ReactMarkdown>
+          }
+        </div>
+      )}
     </motion.div>
   );
 }
