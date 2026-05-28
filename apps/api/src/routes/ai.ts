1d719cf9fed84fec133554ae446b4819dcb36ce4
yuvanvk <abhivigneshofficial@gmail.com>
2026-05-28 20:04:14 +0530

    fix: routes should use DO

--- apps/api/src/routes/ai.ts
+++ apps/api/src/routes/ai.ts
@@ -1,80 +1,94 @@
 import { Hono } from "hono";
 import { eq } from "drizzle-orm";
-import { ChatSchema } from "@/zod";
-import { redis } from "@/services/redis";
 import { Bindings, Variables } from "@/types";
-import { conversation, message as messageTable } from "@workspace/db";
+import { conversation } from "@workspace/db";
 
 const router = new Hono<{ Bindings: Bindings; Variables: Variables }>({
   strict: false,
 });
 
 router.get("/conversations", async (c) => {
-  const userId = c.var.session?.session.userId;
+  const userId = "VCY2XaUNgKPbmcmkEuwAiyfLy10W6L5A";
   const db = c.get("db");
 
   const conversations = await db.query.conversation.findMany({
     where: eq(conversation.userId, userId!),
   });
+
   if (conversations.length === 0) {
-    c.status(404);
-    return c.json({ message: "No conversations found" });
+    return c.json(
+      { message: "No conversations found", data: { conversations: [] } },
+      404,
+    );
   }
 
-  return c.json({ message: "Success", conversations }, 200);
+  return c.json({ message: "OK", data: { conversations } }, 200);
 });
 
 router.get("/conversations/:conversationId", async (c) => {
   const db = c.get("db");
 
-  const { conversationId } = c.req.param();
-  if (!conversationId) {
-    return c.json({ message: "Invalid Inputs" }, 400);
-  }
-
-  let cachedConversation = await redis.get(`conv:${conversationId}`);
-
-  if (cachedConversation) {
-    cachedConversation = JSON.parse(cachedConversation as string);
-    return c.json(
-      { message: "Success", conversation: cachedConversation },
-      200,
-    );
-  }
-
-  const existingConversation = await db.query.conversation.findFirst({
+  const conversationId = c.req.param("conversationId");
+  const validConversation = await db.query.conversation.findFirst({
     where: eq(conversation.id, conversationId),
   });
 
-  if (!existingConversation) {
-    return c.json({ message: "Not found" }, 404);
+  if (!validConversation) {
+    return c.json(
+      { message: "Invalid Conversation", data: { conversation: [] } },
+      404,
+    );
   }
 
-  let existingConversationMessages = await db.query.message.findMany({
-    where: eq(messageTable.conversationId, existingConversation.id),
-  });
+  const id = c.env.CONVERSATION.idFromName(conversationId);
+  const stub = c.env.CONVERSATION.get(id);
 
-  const formattedMessages = existingConversationMessages.map((msg) => ({
-    role: msg.role,
-    content: msg.content,
-  })) as { role: string; content: string }[];
+  const messages = await stub.getMessages();
 
-  await redis.set(`conv:${conversationId}`, JSON.stringify(formattedMessages), {
-    ex: 7200,
-  });
+  if (!messages) {
+    return c.json(
+      { message: "No Messages found", data: { messages: [] } },
+      404,
+    );
+  }
 
-  return c.json({ message: "Success", conversation: formattedMessages }, 200);
+  return c.json({ message: "OK", data: { messages } }, 200);
 });
+
+router.post("/create", async (c) => {
+  const db = c.get("db");
+  const conversationId = crypto.randomUUID();
+
+  await db.insert(conversation).values({
+    id: conversationId,
+    userId: "VCY2XaUNgKPbmcmkEuwAiyfLy10W6L5A",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+
+  return c.json({ message: "Ok" , data: { conversationId } });
+})
 
 router.get("/chat", async (c) => {
   try {
     const db = c.get("db");
 
     let { conversationId } = c.req.query();
-    conversationId = conversationId ?? crypto.randomUUID();
 
-    const id = c.env.CONVERSATION.idFromName(conversationId)
-    const stub = c.env.CONVERSATION.get(id)
+    if(!conversationId) {
+      return c.json({ message: "Conversation id is required"}, 400)
+    }
+
+    const existing = await db.query.conversation.findFirst({
+      where: eq(conversation.id, conversationId),
+    });
+  
+    if (!existing) {
+      return c.json({ message: "Conversation not found" }, 404);
+    }
+
+    const id = c.env.CONVERSATION.idFromName(conversationId);
+    const stub = c.env.CONVERSATION.get(id);
 
     return stub.fetch(c.req.raw);
   } catch (error) {
@@ -83,4 +97,4 @@ router.get("/chat", async (c) => {
   }
 });
 
-export default router
+export default router;
