7ce905d8963cf87d549934a2528ebd46d108b0a4
yuvanvk <abhivigneshofficial@gmail.com>
2026-05-28 20:04:58 +0530

    remove: external dependencies bindings

--- apps/api/src/types.ts
+++ apps/api/src/types.ts
@@ -6,14 +6,11 @@ export type Bindings = {
     D1_DATABASE: D1Database,
     AI: Ai,
     CONVERSATION: DurableObjectNamespace<Conversation>
-    OPENROUTER_API_KEY: string,
     BETTER_AUTH_URL: string,
     BETTER_AUTH_SECRET: string,
     GOOGLE_CLIENT_ID: string,
     GOOGLE_CLIENT_SECRET: string,
     RESEND_API_KEY: string,
-    UPSTASH_REDIS_REST_URL: string,
-    UPSTASH_REDIS_REST_TOKEN: string
 }
 
 type AuthInstance = ReturnType<typeof auth>;
