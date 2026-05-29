c3e12c890538680c44b8815816a6b46318737fe0
yuvanvk <abhivigneshofficial@gmail.com>
2026-05-29 18:54:37 +0530

    feat: add modelName

--- apps/www/store/useModel.tsx
+++ apps/www/store/useModel.tsx
@@ -1,11 +1,14 @@
 import { create } from "zustand";
 
-
 interface ModelStore {
-    model: string,
-    setModel: (model: string) => void
+  modelId: string;
+  modelName: string;
+  setModel: (model: string) => void;
+  setModelName: (name: string) => void;
 }
 export const useModel = create<ModelStore>((set) => ({
-    model: "",
-    setModel: (model) => set({ model })
-}))
+  modelId: "@cf/moonshotai/kimi-k2",
+  modelName: "Kimi k2",
+  setModel: (modelId) => set({ modelId }),
+  setModelName: (name) => set({ modelName: name }),
+}));
