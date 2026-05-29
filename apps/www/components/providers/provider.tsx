551270fbe0be517cf62c10492f74f47a7cd2869f
yuvanvk <abhivigneshofficial@gmail.com>
2026-05-29 18:56:02 +0530

    feat: icons of models

--- apps/www/components/providers/provider.tsx
+++ apps/www/components/providers/provider.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { ThemeProvider } from "./theme-provider";
+import { TooltipProvider } from "@workspace/ui/components/tooltip";
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute={"class"} defaultTheme="system" enableSystem>
-      {children}
+      <TooltipProvider>
+        {children}
+      </TooltipProvider>
     </ThemeProvider>
   );
 }
