1c728a76f6dc526688773f4b40530486dd86f5fe
yuvanvk <abhivigneshofficial@gmail.com>
2026-05-29 11:17:16 +0530

    feat: Conversation Store

---
+++ apps/www/store/useConversation.tsx
@@ -0,0 +1,30 @@
+import { Conversation } from "@workspace/types";
+import { create } from "zustand";
+import { toast } from "sonner";
+
+interface ConversationStore {
+  conversations: Conversation[];
+  setConversations: (conversations: Conversation[]) => void;
+  setTitle: (title: string, id: string) => void;
+  fetch: () => Promise<void>;
+}
+
+export const useConversationStore = create<ConversationStore>((set, get) => ({
+  conversations: [],
+  setConversations: (conversations) => set({ conversations }),
+  setTitle: (title: string, id: string) =>
+    set({
+      conversations: get().conversations.map((conversation) =>
+        conversation.id === id ? { ...conversation, title } : conversation,
+      ),
+    }),
+
+  fetch: async () => {
+    const response = await fetch(`http://localhost:8787/api/v1/ai/conversations`);
+    const { message, data } = await response.json();
+    if (response.status !== 200) {
+      toast.error(message)
+    }
+    set({ conversations: data.conversations ?? [] });
+  },
+}));
