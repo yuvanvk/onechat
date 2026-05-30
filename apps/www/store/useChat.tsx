cce7d684e9b30936aa1b22eb26ac521f96ea9110
yuvanvk <abhivigneshofficial@gmail.com>
2026-05-30 15:36:00 +0530

    feat: add pendingConversation for new chat

--- apps/www/store/useChat.tsx
+++ apps/www/store/useChat.tsx
@@ -1,20 +1,28 @@
 import { create } from "zustand";
-import { Message } from "@workspace/types";
+import { Message, WebSocketCreateStreamMessage } from "@workspace/types";
 
 interface ChatStore {
+  conversationId: string;
   messages: Message[];
+  pendingMessage: WebSocketCreateStreamMessage | null;
   setMessages: (messages: Message[]) => void;
+  setPendingMessage: (message: WebSocketCreateStreamMessage | null) => void;
   addMessage: (message: Message) => void;
   updateMessage: (opts: {
     token?: string;
     id?: string;
     setId?: string;
   }) => void;
+  setConversationId: (conversationId: string) => void;
 }
 
 export const useChatStore = create<ChatStore>((set, get) => ({
+  conversationId: "",
   messages: [],
   setMessages: (messages) => set({ messages }),
+  pendingMessage: null,
+  setPendingMessage: (message) => set({ pendingMessage: message }),
+  setConversationId: (conversationId) => set({ conversationId }),
   addMessage: (message) => set({ messages: [...get().messages, message] }),
   updateMessage: ({ token, id, setId }) =>
     set({
@@ -31,4 +39,3 @@ export const useChatStore = create<ChatStore>((set, get) => ({
           : get().messages,
     }),
 }));
-
