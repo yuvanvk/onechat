51bc89bcedb80cdccd49a10b4962f4dd74549e98
yuvanvk <abhivigneshofficial@gmail.com>
2026-06-04 12:09:00 +0530

    feat: upload objects from client to r2

---
+++ apps/www/lib/s3/client.ts
@@ -0,0 +1,15 @@
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const S3 = new S3Client({
+  region: "auto",
+  endpoint: process.env.R2_API_ENDPOINT,
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
+  },
+});
---
+++ apps/www/utils/upload-to-bucket.ts
@@ -0,0 +1,28 @@
+export async function uploadToBucket(file: File) {
+  try {
+    const response = await fetch(`http://localhost:8787/api/v1/r2`, {
+      method: "POST",
+      body: JSON.stringify({
+        name: file.name,
+        type: file.type,
+        size: file.size,
+      }),
+    });
+
+    if (!response.ok) throw new Error("Failed to get presigned URL");
+    const { data } = await response.json();
+
+    const uploadRes = await fetch(data.signedUrl, {
+      method: "PUT",
+      body: file,
+      headers: { "Content-Type": file.type },
+    });
+
+    if (!uploadRes.ok) throw new Error("Failed to upload to R2");
+
+    return true;
+  } catch (error) {
+    console.error("Upload failed", error);
+    throw error;
+  }
+}
