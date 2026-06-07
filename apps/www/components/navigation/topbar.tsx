5264371f3056f1c7868311fe143c83cb861eb53b
yuvanvk <abhivigneshofficial@gmail.com>
2026-06-07 18:43:28 +0530

    fix: only render chat actions on /c/[id] page

--- apps/www/components/navigation/topbar.tsx
+++ apps/www/components/navigation/topbar.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { cn } from "@workspace/ui/lib/utils";
 import { Forward, Trash2 } from "lucide-react";
-import { useParams, useRouter } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 import { Button } from "@workspace/ui/components/button";
 import { Spinner } from "@workspace/ui/components/spinner";
 import {
@@ -35,6 +35,8 @@ export function Topbar() {
   const { setMessages } = useChatStore();
   const [loading, setLoading] = useState(false);
   const [shareLink, setshareLink] = useState<string>("");
+  const path = usePathname();
+  console.log(path);
 
   async function handleDelete() {
     if (!id) return;
@@ -57,27 +59,28 @@ export function Topbar() {
   }
 
   async function handleShareLinkGeneration() {
-    if(!id) return;
-    setLoading(true)
+    if (!id) return;
+    setLoading(true);
     try {
-      const response = await fetch(`http://localhost:8787/api/v1/ai/chat/share/${id}`, {
-        method: "POST",
-      })
+      const response = await fetch(
+        `http://localhost:8787/api/v1/ai/chat/share/${id}`,
+        {
+          method: "POST",
+        },
+      );
       const { message, data } = await response.json();
-      setshareLink(data.shareLink)
+      setshareLink(data.shareLink);
     } catch (error) {
-      
     } finally {
       setLoading(false);
     }
   }
 
   async function handleShareLinkCopy() {
-    if(!shareLink) return;
+    if (!shareLink) return;
     try {
       await navigator.clipboard.writeText(shareLink);
-    } catch (error) {
-    }
+    } catch (error) {}
   }
 
   return (
@@ -87,68 +90,78 @@ export function Topbar() {
         "flex items-center gap-1",
       )}
     >
-      <Dialog>
-        <DialogTrigger asChild onClick={handleShareLinkGeneration}>
-          <Button
-            size={"icon-sm"}
-            className={cn(
-              "rounded-full bg-[#2d2d2c] text-neutral-300 border-neutral-700",
-              "hover:text-blue-500 hover:bg-blue-100 hover:border-blue-400",
-            )}
-          >
-            <Forward />
-          </Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Share this conversation</DialogTitle>
-            <DialogDescription>
-              A shareable link is ready. Copy it and send it to anyone to give
-              them view access.
-            </DialogDescription>
-          </DialogHeader>
+      {path.startsWith("/c/") && (
+        <>
+          <Dialog>
+            <DialogTrigger asChild onClick={handleShareLinkGeneration}>
+              <Button
+                size={"icon-sm"}
+                className={cn(
+                  "rounded-full bg-[#2d2d2c] text-neutral-300 border-neutral-700",
+                  "hover:text-blue-500 hover:bg-blue-100 hover:border-blue-400",
+                )}
+              >
+                <Forward />
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Share this conversation</DialogTitle>
+                <DialogDescription>
+                  A shareable link is ready. Copy it and send it to anyone to
+                  give them view access.
+                </DialogDescription>
+              </DialogHeader>
 
-          <Input value={shareLink} disabled />
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            {loading ? <Spinner /> : <Button variant="outline" onClick={handleShareLinkCopy}>Copy</Button>}
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+              <Input value={shareLink} disabled />
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogClose>
+                {loading ? (
+                  <Spinner />
+                ) : (
+                  <Button variant="outline" onClick={handleShareLinkCopy}>
+                    Copy
+                  </Button>
+                )}
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
 
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button
-            size={"icon-sm"}
-            className={cn(
-              "rounded-full bg-[#2d2d2c] text-neutral-300  border-neutral-700",
-              "hover:text-rose-500 hover:bg-rose-100 hover:border-rose-400",
-            )}
-          >
-            <Trash2 />
-          </Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogTitle>
-            Are you sure to delete this Conversation?
-          </AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete this
-            Conversation session.
-          </AlertDialogDescription>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction
-              onClick={handleDelete}
-              className={cn("bg-red-500 text-white")}
-            >
-              {loading ? <Spinner /> : "Delete"}
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button
+                size={"icon-sm"}
+                className={cn(
+                  "rounded-full bg-[#2d2d2c] text-neutral-300  border-neutral-700",
+                  "hover:text-rose-500 hover:bg-rose-100 hover:border-rose-400",
+                )}
+              >
+                <Trash2 />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogTitle>
+                Are you sure to delete this Conversation?
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete this
+                Conversation session.
+              </AlertDialogDescription>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                  onClick={handleDelete}
+                  className={cn("bg-red-500 text-white")}
+                >
+                  {loading ? <Spinner /> : "Delete"}
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </>
+      )}
     </div>
   );
 }
