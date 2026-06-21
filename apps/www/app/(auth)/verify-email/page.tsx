22dd689abba77ba1182646cd1a66946420d3fcfc
yuvanvk <abhivigneshofficial@gmail.com>
2026-06-21 15:05:39 +0530

    fix: wrap verify-email inside suspense

--- apps/www/app/(auth)/verify-email/page.tsx
+++ apps/www/app/(auth)/verify-email/page.tsx
@@ -1,5 +1,7 @@
 import { VerifyEmail } from "@/components/auth/verify-email";
-
+import { Suspense } from "react";
 export default function VerifyEmailPage() {
-    return <VerifyEmail />
+    return <Suspense fallback={null}>
+      <VerifyEmail />
+    </Suspense>
 }
