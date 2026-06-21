9b34435e64dc456e6538d0d41d290e09851bc6ce
yuvanvk <abhivigneshofficial@gmail.com>
2026-06-21 15:38:42 +0530

    remove: better-auth advanced settings

--- packages/better-auth/src/better-auth.ts
+++ packages/better-auth/src/better-auth.ts
@@ -94,14 +94,6 @@ export const auth = (env: Bindings) => {
         options,
       ),
     ],
-    advanced: {
-      defaultCookieAttributes: {
-        sameSite: env.ENVIRONMENT === "production" ? "None" : "Lax",
-        secure: env.ENVIRONMENT === "production",
-        partitioned: env.ENVIRONMENT === "production",
-        httpOnly: true,
-      },
-    }
   });
 };
 
